43359553823960a725de152a1797627837c92a8c
Darshit-parzival <darshitrakhasia@gmail.com>
2025-03-09 16:21:29 +0530

    index page done design

--- index.php
+++ index.php
@@ -25,37 +25,57 @@
                 <!-- YouTube Videos -->
                 <iframe class="mt-3" width="300" height="200" src="https://www.youtube.com/embed/kVnLeDOBlHc?si=fXyjEdpY6T0AHt6x" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 <iframe class="mt-3" width="300" height="200" src="https://www.youtube.com/embed/kVnLeDOBlHc?si=fXyjEdpY6T0AHt6x" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+                <!-- Join Us Heading -->
+                <h4 class="mt-5">Join on</h4>
+
+                <!-- Social Icons -->
+                <div class="d-flex flex-wrap justify-content-center mt-2 mb-2" style="max-width: 200px;">
+                    <div class="d-flex justify-content-center w-100 gap-5">
+                        <a href="https://www.facebook.com/AlakhGod" target="_blank"><img src="assets/img/Facebook.png" alt="Facebook" width="40"></a>
+                        <a href="https://www.youtube.com/@AlakhGod" target="_blank"><img src="assets/img/youtube.jpeg" alt="YouTube" width="40"></a>
+                        <a href="https://www.instagram.com/AlakhGod/" target="_blank"><img src="assets/img/instagram.png" alt="Instagram" width="40"></a>
+                    </div>
+                    <div class="d-flex justify-content-center w-100 gap-5 mt-2">
+                        <a href="https://www.pinterest.com/alakhgod" target="_blank"><img src="assets/img/pinterest-1.png" alt="Pinterest" width="40"></a>
+                        <a href="https://x.com/AlakhGod" target="_blank"><img src="assets/img/X.png" alt="Twitter" width="40"></a>
+                        <a href="https://www.linkedin.com/in/AlakhGod" target="_blank"><img src="assets/img/Linkedin.png" alt="LinkedIn" width="40"></a>
+                    </div>
+                    <div class="d-flex justify-content-center w-100 gap-3 mt-2">
+                        <a href="https://alakhgod.medium.com/" target="_blank"><img src="assets/img/Medium.png" alt="Medium" width="40"></a>
+                    </div>
+                </div>
+                <a href="https://www.linkedin.com/newsletters/7272962487929446401/?displayConfirmation=true" target="_blank" class="btn btn-primary mt-5">Subscribe to Newsletter<br />Message From God</a>
             </div>
 
-        </div>
 
-        <!-- Get to Know God Section -->
-        <div class="container text-center mt-5 mb-5">
-            <h2 class="section-heading">Get to Know God and Connect With God</h2>
-            <div class="d-flex flex-wrap justify-content-center mt-4">
-                <!-- Subcontent 1 -->
-                <div class="info-box">
-                    <h4>My Book</h4>
-                    <p>Until August 2023, you had to attend a 3-day workshop by My Messenger, Pradip Mukherji, to get to know Me. Today, you can know Me through My Book.</p>
-                    <a href="#" class="btn btn-primary">Click Here to Know About My Book</a>
-                </div>
+            <!-- Get to Know God Section -->
+            <div class="container text-center mt-5 mb-5">
+                <h2 class="section-heading">Get to Know God and Connect With God</h2>
+                <div class="d-flex flex-wrap justify-content-center mt-4">
+                    <!-- Subcontent 1 -->
+                    <div class="info-box">
+                        <h4>My Book</h4>
+                        <p>Until August 2023, you had to attend a 3-day workshop by My Messenger, Pradip Mukherji, to get to know Me. Today, you can know Me through My Book.</p>
+                        <a href="#" class="btn btn-primary">Click Here to Know About My Book</a>
+                    </div>
 
-                <!-- Subcontent 2 -->
-                <div class="info-box">
-                    <h4>My Mantra</h4>
-                    <p>My Mantra takes less than 2 mins a day to do. It is always given for FREE. While I will not give you what you want, yet I can smoothen out your life and heal you in unexpected ways. Just do it for 30 days.</p>
-                    <a href="#" class="btn btn-primary">Click Here To Receive My Mantra</a>
-                </div>
+                    <!-- Subcontent 2 -->
+                    <div class="info-box">
+                        <h4>My Mantra</h4>
+                        <p>My Mantra takes less than 2 mins a day to do. It is always given for FREE. While I will not give you what you want, yet I can smoothen out your life and heal you in unexpected ways. Just do it for 30 days.</p>
+                        <a href="#" class="btn btn-primary">Click Here To Receive My Mantra</a>
+                    </div>
 
-                <!-- Subcontent 3 -->
-                <div class="info-box">
-                    <h4>God’s Healing</h4>
-                    <p>Talk to the above Photo about your suffering, pain, problems, issues, negativity, confusion, disease, neurosis, challenges, fear, etc. You will be pleasantly surprised with whatever is delivered.</p>
-                    <a href="#" class="btn btn-primary">Click to Receive God’s Healing through Photo</a>
+                    <!-- Subcontent 3 -->
+                    <div class="info-box">
+                        <h4>God’s Healing</h4>
+                        <p>Talk to the above Photo about your suffering, pain, problems, issues, negativity, confusion, disease, neurosis, challenges, fear, etc. You will be pleasantly surprised with whatever is delivered.</p>
+                        <a href="#" class="btn btn-primary">Click to Receive God’s Healing through Photo</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<?php include('footer.php'); ?>
+    <?php include('footer.php'); ?>
